07f640e39d36d7d6e82ee835e553908f353f6da6
Nem Greene <vincent.r.greene@gmail.com>
2022-10-18 15:19:08 +0100

    take 2

--- src/Add.js
+++ src/Add.js
@@ -1,43 +1,107 @@
-import React from 'react';
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
 
+function Add(props) {
+  const [formValues, changeFormValues] = useState({
+    id: 0,
+    task: "",
+    complete: false,
+  });
 
-function Add(){
-  
+  toastr.options = {
+    closeButton: false,
+    debug: false,
+    newestOnTop: false,
+    progressBar: true,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
+
+  // handle chnaging the form
+  const handleChange = (event) => {
+    let newVal = event.target.value;
+    let keyToChange = event.target.name;
+    const newState = { ...formValues };
+
+    if (event.target.name === "complete") {
+      newState[keyToChange] = !formValues.complete;
+    } else {
+      newState[keyToChange] = newVal;
+    }
+    changeFormValues(newState);
+  };
+
+  const submitHandler = () => {
+    props.changeTodos((prevState) => {
+      // make a shallow duplicate of the prev state
+      let newState = [...prevState, formValues];
+      return newState;
+    });
+    toastr["success"]("Todo added", "Success");
+  };
+
   return (
-      <div>
-        <Form>
-          <Form.Group controlId="taskID">
-            <Form.Label> Task ID</Form.Label>
-            <Form.Control 
-              name="id" 
-              type="number" 
-            />
-          </Form.Group>
+    <div>
+      <Form>
+        <Form.Group controlId="taskID">
+          <Form.Label> Task ID</Form.Label>
+          <Form.Control
+            name="id"
+            value={formValues.id}
+            type="number"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+        </Form.Group>
 
-          <Form.Group controlId="taskDescription">
-            <Form.Label> Description</Form.Label>
-            <Form.Control 
-              name="description" 
-              type="text" 
-            />
-          </Form.Group>
+        <Form.Group controlId="taskDescription">
+          <Form.Label> Description</Form.Label>
+          <Form.Control
+            value={formValues.task}
+            name="task"
+            type="text"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+        </Form.Group>
 
-          <Form.Group controlId="complete">
-            <Form.Check 
-              type="checkbox"
-              id="complete"
-              label="Completed?"
-            />
-          </Form.Group>
+        <Form.Group controlId="complete">
+          <Form.Check
+            type="checkbox"
+            name="complete"
+            checked={formValues.checked}
+            id="complete"
+            label="Completed?"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+        </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        <Button
+          variant="primary"
+          onClick={() => {
+            submitHandler();
+          }}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
-
 }
 export default Add;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import './App.css';
-import View from './View'
-import Add from './Add';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import "./App.css";
+import View from "./View";
+import Add from "./Add";
 
-function App(){
-
-    return (
-        <Container>
-          <View />
-          <Add />
-        </Container>
-    );
-
+function App() {
+  // app will track a list of all the todos
+  const [todos, changeTodos] = useState([
+    { id: 1, task: "make static data", complete: false },
+    { id: 2, task: "make dynamic data", complete: false },
+    { id: 3, task: "remove duplicate data", complete: true },
+  ]);
+  // through props, pass a function to add, to add props to the list
+  // through props, pass the list os posts to view
+  return (
+    <Container>
+      <View todos={todos} />
+      <Add changeTodos={changeTodos} />
+    </Container>
+  );
 }
 export default App;
